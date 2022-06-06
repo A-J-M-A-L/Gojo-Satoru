a607ec666cb7b3d84cdb024cfc5a3f8ef6513538
A-J-M-A-L <98047253+A-J-M-A-L@users.noreply.github.com>
2022-06-06 20:29:06 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
+global.owner = ['918590508376']
 global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
+global.ownernomer = '918590508376'
 global.ownername = '🐦Star Boy Nexus'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
